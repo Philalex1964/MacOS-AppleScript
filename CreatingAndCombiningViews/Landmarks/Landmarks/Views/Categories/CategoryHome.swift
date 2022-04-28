87efff91982698e7fd31b7cd3dd3ab9e559bfc98
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-28 14:07:41 +0200

    Add a Category View

---
+++ CreatingAndCombiningViews/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Aleksandar Filipov on 4/28/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
