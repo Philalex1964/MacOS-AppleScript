f359275fb154c6c42d9284bcf207f3270aec30d8
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-29 09:55:59 +0200

    Define the Profile Editor

--- CreatingAndCombiningViews/Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ CreatingAndCombiningViews/Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -22,9 +22,8 @@ struct ProfileHost: View {
             if editMode?.wrappedValue == .inactive {
                 ProfileSummary(profile: modelData.profile)
             } else {
-                Text("Profile Editor")
+                ProfileEditor(profile: $draftProfile)
             }
-
         }
         .padding()
     }
