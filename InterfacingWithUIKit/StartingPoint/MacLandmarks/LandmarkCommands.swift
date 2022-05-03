f1505e9407f7b5ba7eaedcc92dc31b2606122bbf
Alexander Filippov <philalex196401@gmail.com>
2022-05-03 22:23:30 +0200

    Add a Built-in Menu Command

---
+++ InterfacingWithUIKit/StartingPoint/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,15 @@
+//
+//  LandmarkCommands.swift
+//  WorkingWithUIControls
+//
+//  Created by Alexander Filippov on 3.5.22..
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
