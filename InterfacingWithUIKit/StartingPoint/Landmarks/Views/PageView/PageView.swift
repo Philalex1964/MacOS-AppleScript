ee4793bd756f194fdca4597f2b289ce7e2bbca79
Alexander Filippov <philalex196401@gmail.com>
2022-05-03 15:51:01 +0200

    Add a Custom Page Control

--- InterfacingWithUIKit/StartingPoint/Landmarks/Views/PageView/PageView.swift
+++ InterfacingWithUIKit/StartingPoint/Landmarks/Views/PageView/PageView.swift
@@ -11,13 +11,14 @@ import SwiftUI
 struct PageView<Page: View>: View {
     @State private var currentPage = 0
     var pages: [Page]
-
+    
     var body: some View {
-        VStack {
-                    PageViewController(pages: pages, currentPage: $currentPage)
-                    
-            Text("Current Page: \(currentPage)")
-                }
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
     }
 }
 
