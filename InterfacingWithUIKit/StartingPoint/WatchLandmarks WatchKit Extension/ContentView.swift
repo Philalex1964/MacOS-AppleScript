5c27caf6426f55fc32665f2008bed83188aeecd1
Alexander Filippov <philalex196401@gmail.com>
2022-05-03 16:14:01 +0200

    Add a watchOS Target

---
+++ InterfacingWithUIKit/StartingPoint/WatchLandmarks WatchKit Extension/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Alexander Filippov on 3.5.22..
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InterfacingWithUIKit/StartingPoint/WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Alexander Filippov on 3.5.22..
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
