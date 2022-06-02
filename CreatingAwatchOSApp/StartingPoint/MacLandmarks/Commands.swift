09ac136609efe3595003f28a0afea2745c7c23cc
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-06-02 12:34:39 +0200

    Add a Built-in Menu Command

---
+++ CreatingAwatchOSApp/StartingPoint/MacLandmarks/Commands.swift
@@ -0,0 +1,15 @@
+//
+//  Commands.swift
+//  InterfacingWithUIKit
+//
+//  Created by Aleksandar Filipov on 6/2/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
