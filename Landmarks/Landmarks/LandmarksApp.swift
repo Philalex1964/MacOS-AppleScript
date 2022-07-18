69f3ef3c19d8ca46e9002ea581e5b8028f91e869
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-07-18 11:44:23 +0200

    Created a new Landmarks SwiftUI project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Aleksandar Filipov on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
